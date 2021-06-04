987da0f403d61f66bf6e2ddf18f637128efbef56
Raoul Shah <74040700+Raoul-Shah@users.noreply.github.com>
2021-06-04 23:05:23 +0200

    Added scroll-top

--- app.js
+++ app.js
@@ -1,1 +1,55 @@
+$(document).ready(function(){
 
+    $('#menu-bar').click(function(){
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+
+    $(window).on('load scroll',function(){
+
+        $('#menu-bar').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+
+        $('section').each(function(){
+
+            let top = $(window).scrollTop();
+            let height = $(this).height();
+            let id = $(this).attr('id');
+            let offset = $(this).offset().top - 200;
+
+            if(top > offset && top < offset + height){
+                $('.navbar ul li a').removeClass('active');
+                $('.navbar').find(`[href="#${id}"]`).addClass('active');
+            }
+
+        });
+
+    });
+
+    $('.list .btn').click(function(){
+        $(this).addClass('active').siblings().removeClass('active');
+        let src = $(this).attr('data-src');
+        $('.menu .row .image img').attr('src',src);
+    });
+
+         //scroll spy 
+
+         $('section').each(function(){
+
+            let height = $(this).height();
+            let offset = $(this).offset().top - 200;
+            let id = $(this).attr('id');
+            let top = $(window).scrollTop();
+    
+            if(top > offset && top < offset + height){
+                $('.navbar ul li a').removeClass('active')
+                $('.navbar').find(`[href="#${id}"]`).addClass('active');
+            }
+    
+         });   
+
+});
+
+
+
+    
